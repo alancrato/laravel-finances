37b9e9ed9bf70bb627426f2001e190053963dc32
Alan Rodrigues <alancrato@hotmail.com>
2018-03-21 14:29:38 -0300

    add favicon e config title

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,6 +21,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $title = 'Laravel - Office Management!';
+        return view('home', compact('title'));
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,17 +8,14 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ $title or 'Laravel - Office Management!' }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ url('css/style.css')}} " rel="stylesheet">
     <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
-    <style>
-        ul li{
-            list-style-type: none;
-        }
-    </style>
+    <link rel="icon" type="image/png" href="https://utils.organizze.com.br/favicons/favicon-32x32.png" sizes="32x32">
+
 </head>
 <body>
     <div id="app">
